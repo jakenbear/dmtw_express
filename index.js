ea29cd48de4b5d1060037177bbfd3a2db5566c50
Jake Reardon <jakenbear@gmail.com>
2025-08-01 14:33:57 -0400

    Update port for render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
